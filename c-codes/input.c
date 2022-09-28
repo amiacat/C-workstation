a4f2be48f0f677f8bfcc9464e875b41db2792632
CatisHacker <cutec7782@gmail.com>
2022-09-28 00:54:19 +0000

    sussy baka #2

--- c-codes/input.c
+++ c-codes/input.c
@@ -2,10 +2,14 @@
 
 int main(){
     int age;
-    
+    char name[10];
+
     printf("What is your age?: ");
     scanf("%d", &age);
-    printf("Your age is %d \n", age);
+    printf("\nAnd what is your name?: ");
+    fflush(stdin);
+    fgets(name, 10, stdin);
+    printf("Your age is %d, and your name is %s \n", age, name);
     
     return 0;
 }
